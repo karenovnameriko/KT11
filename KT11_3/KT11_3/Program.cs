ffbb64dc8a86388244796e1140f76fb9923b8b68
karenovnameriko <meri.terteryan.2007@mail.ru>
2025-12-16 21:16:38 +0300

    Add prokects

---
+++ KT11_3/KT11_3/Program.cs
@@ -0,0 +1,62 @@
+﻿//Напишите обобщенный интерфейс IPrintable<T>, который содержит метод void Print(), который выводит на консоль информацию об объекте типа T.
+//Затем напишите ограничение для этого интерфейса, чтобы он мог работать только с типами, которые являются ссылочными типами (class) или
+//типами значений (struct), но не с указателями (pointer) или динамическими типами (dynamic). Затем напишите классы Student и Vector, которые\
+//реализуют этот интерфейс и выводят на консоль свои свойства, такие как Name, Age, Grade, X, Y и Z. Затем напишите пример использования этого
+//интерфейса для печати объектов этих классов.
+namespace KT11_3;
+
+class Program
+{
+    interface IPrintable<T> where T: notnull
+    {
+        void Print();
+    }
+
+    class Student: IPrintable<Student>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string name, int age, double grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Student: Name = {Name}, Age = {Age}, Grade = {Grade}");
+        }
+
+    }
+    class Vector : IPrintable<Vector>
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Vector: X = {X}, Y = {Y}, Z = {Z}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Student s = new Student("Anna", 20, 4.5);
+        s.Print(); 
+
+        
+        Vector v = new Vector(1.0, 2.0, 3.0);
+        v.Print();  
+    }
+}
+
